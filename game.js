343931fd2a309a1eb7bd3156e2ab60f36689dbea
Ajay Bhattacharyya <33659124+AjayBhatta16@users.noreply.github.com>
2020-11-19 23:34:33 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,45 +1,40 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
-import { update as updateFood, draw as drawFood } from './food.js'
-import { outsideGrid } from './grid.js'
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js';
+import {update as updateFood, draw as drawFood} from './food.js';
+import {outsideGrid} from './grid.js';
 
-let lastRenderTime = 0
-let gameOver = false
-const gameBoard = document.getElementById('game-board')
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
-  if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
+    if(gameOver) {
+        if(confirm('You lost. Press ok to restart.')) {
+            window.location = '/snakegame';
+        }
+        return;
     }
-    return
-  }
-
-
-  window.requestAnimationFrame(main)
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-
-
-  lastRenderTime = currentTime
-
-  update()
-  draw()
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+    lastRenderTime = currentTime;
+    update();
+    draw();
 }
 
-window.requestAnimationFrame(main)
+window.requestAnimationFrame(main);
 
 function update() {
-  updateSnake()
-  updateFood()
-  checkDeath()
+    updateSnake();
+    updateFood();
+    checkDeath();
 }
 
 function draw() {
-  gameBoard.innerHTML = ''
-  drawSnake(gameBoard)
-  drawFood(gameBoard)
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
 }
 
 function checkDeath() {
-  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
