3cdc1ee1bb34703c5746ac77e5b20f9f579b1478
Ajay Bhattacharyya <33659124+AjayBhatta16@users.noreply.github.com>
2020-11-19 23:33:50 -0500

    Update food.js

--- food.js
+++ food.js
@@ -1,28 +1,28 @@
-import { onSnake, expandSnake } from './snake.js'
-import { randomGridPosition } from './grid.js'
+import {onSnake,expandSnake} from './snake.js';
+import {randomGridPosition} from './grid.js';
 
-let food = getRandomFoodPosition()
-const EXPANSION_RATE = 5
+let food = randomGridPosition();
+const EXPANSION_RATE = 1;
 
 export function update() {
-  if (onSnake(food)) {
-    expandSnake(EXPANSION_RATE)
-    food = getRandomFoodPosition()
-  }
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE);
+        food = getRandomFoodPosition();
+    }
 }
 
 export function draw(gameBoard) {
-  const foodElement = document.createElement('div')
-  foodElement.style.gridRowStart = food.y
-  foodElement.style.gridColumnStart = food.x
-  foodElement.classList.add('food')
-  gameBoard.appendChild(foodElement)
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
 }
 
 function getRandomFoodPosition() {
-  let newFoodPosition
-  while (newFoodPosition == null || onSnake(newFoodPosition)) {
-    newFoodPosition = randomGridPosition()
-  }
-  return newFoodPosition
+    let newFoodPosition;
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition();
+    }
+    return newFoodPosition;
 }
