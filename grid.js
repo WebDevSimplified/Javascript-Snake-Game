e80d1f4df727fabde3071d58a0e96bfe4f5d1394
Ajay Bhattacharyya <33659124+AjayBhatta16@users.noreply.github.com>
2020-11-19 23:35:49 -0500

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,15 +1,17 @@
-const GRID_SIZE = 21
+const GRID_SIZE = 21;
 
 export function randomGridPosition() {
-  return {
-    x: Math.floor(Math.random() * GRID_SIZE) + 1,
-    y: Math.floor(Math.random() * GRID_SIZE) + 1
-  }
+    return {
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
+        y: Math.floor(Math.random() * GRID_SIZE) + 1
+    }
 }
 
 export function outsideGrid(position) {
-  return (
-    position.x < 1 || position.x > GRID_SIZE ||
-    position.y < 1 || position.y > GRID_SIZE
-  )
+    return (
+        position.x < 1 
+        || position.x > GRID_SIZE
+        || position.y < 1
+        || position.y > GRID_SIZE
+    );
 }
